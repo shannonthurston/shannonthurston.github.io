842972cdeff548326918296ccca97c28a131404f
shannonthurston <shannonthurston@gmail.com>
2017-11-25 17:11:21 -0700

    new home 1
    created a basic home page with a link to the single page I have available.

---
+++ script/app_myGitHubIO.js
@@ -0,0 +1,11 @@
+var app = angular.module('myGitHubIOApp',[]);
+app.controller('myGitHubIOAppCtrl', function($scope){
+    
+    $scope.data = {};
+    d = $scope.data;
+    d.test = 'test value';
+    
+    
+    
+    
+});
