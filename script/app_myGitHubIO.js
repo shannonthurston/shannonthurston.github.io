3ed9fad7e35fd2249f1a660f0d1215b167d7aef3
shannonthurston <5109752+shannonthurston@users.noreply.github.com>
2017-11-25 21:19:36 -0700

    implement directives

--- script/app_myGitHubIO.js
+++ script/app_myGitHubIO.js
@@ -1,5 +1,5 @@
 var app = angular.module('myGitHubIOApp',[]);
-app.controller('myGitHubIOAppCtrl', function($scope){
+app.controller('myGitHubIOAppCtrl', ['$scope', function($scope){
     
     $scope.data = {};
     d = $scope.data;
@@ -8,4 +8,22 @@ app.controller('myGitHubIOAppCtrl', function($scope){
     
     
     
+}]);
+app.directive("mainCategoryCard", function() {
+
+var tmplt =  "<div class='col-xs-6 col-sm-4'>";
+tmplt += "<div class='thumbnail text-center'>";
+tmplt += "<a href='{{href}}'>{{name}}</a>";
+tmplt += "</div></div>";
+  
+  
+    return {
+        template : tmplt,
+        link: function(scope, elem, attrs) {
+            scope.href = attrs.href;
+            scope.name = attrs.name;
+            
+            console.log('scope.href: ' + scope.href);
+        }
+    };
 });
